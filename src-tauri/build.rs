e7746bfe727326c0b484fb3e74a3aa6877cd11f8
Ardian Eka Candra <ardianoption@gmail.com>
2026-04-30 08:05:39 +0700

    fresh init

---
+++ src-tauri/build.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("cargo:rustc-link-arg=-Wl,-z,max-page-size=16384");
+    println!("cargo:rustc-link-arg=-Wl,-z,common-page-size=16384");
+    tauri_build::build()
+} 
---
+++ src-tauri/gen/android/app/src/main/java/com/sakti_dev/sakti_pos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sakti_dev.sakti_pos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
